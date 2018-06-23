7f77e0a70501b6592ec19c393ec83d4c7a90561a
Mary333 <mariyshirokova11@gmail.com>
2018-06-23 03:48:36 +0300

    Hotfix

--- main.py
+++ main.py
@@ -11,11 +11,5 @@ def fib():
     yield second
     first = first + second
 
-def create_uuid_gen():
-    return (uuid.uuid4() for _ in itertools.count())
-
-print(next(gen_exp))
-print(next(gen_exp))
-
 fib_gen = fib()
 print(list(itertools.islice(fib_gen, 10)))
